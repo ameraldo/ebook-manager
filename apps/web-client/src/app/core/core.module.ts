d1a86b61ab60aa40c02d39d45a35cf2f865dc5f4
ameraldo <ameraldohidri@gmail.com>
2023-09-21 11:32:37 +0200

    polished the library component

--- apps/web-client/src/app/core/core.module.ts
+++ apps/web-client/src/app/core/core.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+// Import the Angular Material modules
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'
+
 import { LibraryComponent } from './library/library.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, LayoutModule, MatGridListModule, MatCardModule],
   declarations: [LibraryComponent],
   exports: [LibraryComponent],
 })
--- apps/web-client/src/app/core/library/library.component.ts
+++ apps/web-client/src/app/core/library/library.component.ts
@@ -5,4 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.scss'],
 })
-export class LibraryComponent {}
+export class LibraryComponent {
+
+  books = [
+    {
+      title: 'Art of War',
+      author: 'Sun Tzu',
+      coverImage: 'https://m.media-amazon.com/images/I/71qbK90zBjL.jpg',
+    },
+    {
+      title: 'The 48 Laws of Power',
+      author: 'Robert Greene',
+      coverImage: 'https://cdn.kobo.com/book-images/48827ce4-6c36-4e5b-949a-c642fc566dce/1200/1200/False/the-48-laws-of-power.jpg',
+    },
+    {
+      title: 'On the Road',
+      author: 'Jack Kerouac',
+      coverImage: 'https://i.pinimg.com/originals/92/b6/c0/92b6c06203fedad753fdebefa2c62624.jpg',
+    },
+    {
+      title: 'The Book of Five Rings',
+      author: 'Miyamoto Musashi',
+      coverImage: 'https://i5.walmartimages.com/asr/be1c0140-2601-4b93-9093-1e53f1e56180_1.c300dd63fe1ad8d003d65fea6e3724cd.jpeg',
+    }
+  ];
+
+}
